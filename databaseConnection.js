714fc0f18c3153423a935e04752e611a72f17858
mangaslave <ademi.jeon@gmail.com>
2024-02-19 13:07:54 -0800

    Initial commit

---
+++ databaseConnection.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2/promise');
+
+const is_qoddi = process.env.IS_QODDI || false;
+
+const dbConfigQoddi = {
+	host: "sql.freedb.tech",
+    user: "freedb_2350_master",
+    password: "fqAmtT8s?8*vrnJ",
+    database: "freedb_comp2350-week2-A01388208",
+    multipleStatements: false,
+};
+
+const dbConfigLocal = {
+	host: "localhost",
+	user: "root",
+	password: "Password",
+	database: "database1",
+	multipleStatements: false
+};
+
+if (is_qoddi) {
+	var database = mysql.createPool(dbConfigQoddi);
+}
+else {
+	var database = mysql.createPool(dbConfigLocal);
+}
+
+module.exports = database;
+		
