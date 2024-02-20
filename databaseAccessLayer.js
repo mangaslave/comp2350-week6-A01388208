42e6d454c4c4d092d5c701e70c3a3fa33e696875
mangaslave <ademi.jeon@gmail.com>
2024-02-19 16:27:17 -0800

    fix adduser

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -54,6 +54,24 @@ return false;
 }
 }
 
+async function deleteUser(webUserId) {
+	let sqlDeleteUser = `
+   DELETE FROM web_user
+   WHERE web_user_id = :userID
+   `;
+	let params = {
+	userID: webUserId
+	};
+	console.log(sqlDeleteUser);
+   try {
+   await database.query(sqlDeleteUser, params);
+   return true;
+   }
+   catch (err) {
+   console.log(err);
+   return false;
+   }
+   }
 
 
-module.exports = {getAllUsers, addUser}
+module.exports = {getAllUsers, addUser, deleteUser}
